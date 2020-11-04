935e45f689e9f469d3f2f3a9181196e6c82795b4
Sirichai <sirichai.b@23perspective.com>
2020-11-05 01:56:59 +0700

    [ Add ] Template

---
+++ MVVM.xctemplate/___FILEBASENAMEASIDENTIFIER___/ViewControllers/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,18 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: User Interaction
+
+    // MARK: Helpers
+
+    func configure() {
+        // configure for passed data between view controller
+    }
+}
