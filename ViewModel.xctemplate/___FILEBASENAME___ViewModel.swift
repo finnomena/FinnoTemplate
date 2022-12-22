676d75221da43c8402d30b33c8ec6ca94f5d5fe5
forzfilm <film@finnomena.com>
2022-12-22 16:48:36 +0700

    Update view model to support new pattern

--- ViewModel.xctemplate/___FILEBASENAME___ViewModel.swift
+++ ViewModel.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -1,19 +1,26 @@
 //  ___FILEHEADER___
 
-protocol ___FILEBASENAMEASIDENTIFIER___Input {
+import RxSwift
+
+protocol ___FILEBASENAMEASIDENTIFIER___Inputs {
 }
 
-protocol ___FILEBASENAMEASIDENTIFIER___Output {
+protocol ___FILEBASENAMEASIDENTIFIER___Outputs {
 }
 
 class ___FILEBASENAMEASIDENTIFIER___ {
 
-    var input: ___FILEBASENAMEASIDENTIFIER___Input { return self }
-    var output: ___FILEBASENAMEASIDENTIFIER___Output { return self }
+    var inputs: ___FILEBASENAMEASIDENTIFIER___Inputs { return self }
+    var outputs: ___FILEBASENAMEASIDENTIFIER___Outputs { return self }
+
+    let isDataReady: PublishSubject<Void> = PublishSubject()
+    let state: PublishSubject<ViewState> = PublishSubject()
+
+    private let disposeBag = DisposeBag()
 }
 
-extension ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Input {
+extension ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Inputs {
 }
 
-extension ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Output {
+extension ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Outputs {
 }
