6d256d3bc896d3b1b11692da888f85c65bb5c5f8
forzfilm <film@finnomena.com>
2022-12-26 10:54:16 +0700

    MOBILITY-1277 | Update XCode template
    - Update view model pattern in mvvm template
    - Update view controller pattern in mvvm template
    - Add view controller and xib template
    - Add networking pattern

---
+++ Networking.xctemplate/___FILEBASENAME___APIClient.swift
@@ -0,0 +1,10 @@
+//  ___FILEHEADER___
+
+import RxSwift
+import SwiftyJSON
+
+protocol ___FILEBASENAMEASIDENTIFIER___Protocol {
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Protocol { 
+}
