018755430aa14b066e16a525a2cf6c23a8902ad3
forzfilm <film@finnomena.com>
2023-01-03 13:03:11 +0700

    MOBILITY-1277 | Update XCode template
    - Add collection view extension

---
+++ XIB+ViewController.xctemplate/ViewController+CollectionView/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,42 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+
+    private var viewModel: ___VARIABLE_productName:identifier___ViewModel = ___VARIABLE_productName:identifier___ViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setObservable()
+        setupUI()
+    }
+
+    // MARK: - User Interaction
+
+    // MARK: - Helpers
+
+    func configure() {
+    }
+
+    private func setObservable() {
+    }
+
+    private func setupUI() {
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {   
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: UICollectionViewDelegate {
+}
